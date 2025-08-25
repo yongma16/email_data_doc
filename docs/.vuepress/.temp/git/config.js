6ffc26d0206f20a20162e195ef5bfb97de586443
mayong <v_bymyma@tencent.com>
2025-08-25 11:33:14 +0800

    Remove .cache and .temp folders from git tracking and update .gitignore

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "G:/front_projects/doc/email_data_doc/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "G:/front_projects/doc/email_data_doc/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
